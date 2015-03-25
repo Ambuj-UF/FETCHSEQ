3e52d5814c2b3f689f2b8de797ebfccf85813131
Ambuj-UF <sendambuj@gmail.com>
2015-03-25 04:30:44 -0400

    dev

--- fetch.py
+++ fetch.py
@@ -177,8 +177,7 @@ def main():
     listObject_human = [x.strip('\n') for x in genes]
     listObject_mouse = None
 
-    #fetcher(listObject_human, listObject_mouse, output=argmnts.o, logObj = "exonName.log")
-
+    fetcher(listObject_human, output=argmnts.o, logObj="exonName.log")
 
 
 if __name__ == "__main__":
